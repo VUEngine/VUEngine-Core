edc676e66f93f4de0af782475df4566fe9aabef3
Jorge Eremiev <jorgech3@gmail.com>
2015-05-03 13:20:10 -0500

    Bug fix: _SINGLE animation not working properly.

--- source/graphics/2d/Textures/Object/ObjectTexture.c
+++ source/graphics/2d/Textures/Object/ObjectTexture.c
@@ -137,7 +137,7 @@ static void ObjectTexture_writeAnimatedSingle(ObjectTexture this)
 		for (; j < cols; j++)
 		{
 			s32 objectIndex = this->objectIndex + i * cols + j;
-			s32 charNumberIndex = objectIndex * 2;
+			s32 charNumberIndex = (i * cols + j) << 1;
 			u16 charNumber = charLocation + (framePointer[charNumberIndex] | (framePointer[charNumberIndex + 1] << 8));
 			OAM[(objectIndex << 2) + 3] = palette | (charNumber & 0x7FF);
 		}
