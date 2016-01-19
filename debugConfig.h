2ee1f61bec27259f13933670dfa0392504797041
Jorge Eremiev <jorgech3@gmail.com>
2016-01-19 11:59:52 -0500

    Added conditional include macros.

--- debugConfig.h
+++ debugConfig.h
@@ -1,26 +1,61 @@
-/* VBJaEngine: bitmap graphics engine for the Nintendo Virtual Boy
- *
- * Copyright (C) 2007 Jorge Eremiev <jorgech3@gmail.com>
- *
- * This program is free software; you can redistribute it and/or modify it under the terms of the GNU
- * General Public License as published by the Free Software Foundation; either version 3 of the License,
- * or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
- * the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public
- * License for more details.
- *
- * You should have received a copy of the GNU General Public License along with this program. If not,
- * see <http://www.gnu.org/licenses/>.
- */
+#ifndef	DEBUG_CONFIG_H_
+#define	DEBUG_CONFIG_H_
 
-#ifndef DEBUG_CONFIG_H_
-#define DEBUG_CONFIG_H_
+#ifdef __DEBUG
+#define __DEBUG_NO_FADE
+#endif
 
+#include <debugUtilities.h>
 
-//---------------------------------------------------------------------------------------------------------
-// 										CONFIG MACROS OVERRIDE
-//---------------------------------------------------------------------------------------------------------
+#define __0_5F_FIX19_13		0x00001000
 
+#define __ALERT_STACK_OVERFLOW
+//#define __DEBUG_NO_FADE
+
+#undef __TIMER_RESOLUTION						
+#define __TIMER_RESOLUTION						10
+
+//#define __DEBUG_NO_FADE
+#define __PRINT_FRAMERATE
+#undef	__FRAME_CYCLE
+#define	__FRAME_CYCLE							0
+
+/*
+#undef __MEMORY_POOL_ARRAYS																			
+#define __MEMORY_POOL_ARRAYS																			\
+	__BLOCK_DEFINITION(188, 1)																			\
+	__BLOCK_DEFINITION(164, 4)																			\
+	__BLOCK_DEFINITION(132, 30)																			\
+	__BLOCK_DEFINITION(112, 52)																			\
+	__BLOCK_DEFINITION(96, 58)																			\
+	__BLOCK_DEFINITION(76, 32)																			\
+	__BLOCK_DEFINITION(68, 75)																			\
+	__BLOCK_DEFINITION(28, 254)																			\
+	__BLOCK_DEFINITION(20, 632)																			\
+	__BLOCK_DEFINITION(16, 290)	
+*/
+
+/*
+// these improve performance in the real machine
+#undef __OPTICS_NORMALIZE
+#define __OPTICS_NORMALIZE(Vector)													\
+	Vector.x -= (_screenPosition->x + 0x00001000)& 0xFFFFE000;						\
+	Vector.y -= (_screenPosition->y + 0x00001000)& 0xFFFFE000;						\
+	Vector.z -= (_screenPosition->z + 0x00001000)& 0xFFFFE000;
+
+#undef __OPTICS_PROJECT_TO_2D
+#define __OPTICS_PROJECT_TO_2D(Vector3D, Vector2D)									\
+		Vector2D.x = Vector3D.x 													\
+			+ (FIX19_13_MULT(_optical->horizontalViewPointCenter - 					\
+					Vector3D.x, Vector3D.z) >> _optical->maximumViewDistancePower);	\
+		Vector2D.y = Vector3D.y 													\
+			- (FIX19_13_MULT(Vector3D.y - _optical->verticalViewPointCenter,		\
+				Vector3D.z) >> _optical->maximumViewDistancePower);					\
+		Vector2D.z = Vector3D.z;													\
+		Vector2D.x += 0x00001000;													\
+		Vector2D.y += 0x00001000;													\
+		Vector2D.x &= 0xFFFFE000;													\
+		Vector2D.y &= 0xFFFFE000;					
+*/
 
 #endif
--- debugUtilities.h
+++ debugUtilities.h
@@ -1,54 +1,6 @@
-#ifdef __DEBUG
-#define __DEBUG_NO_FADE
-#endif
+#ifndef	DEBUG_UTILITIES_H_
+#define	DEBUG_UTILITIES_H_
 
-//#define __ALERT_STACK_OVERFLOW
-//#define __DEBUG_NO_FADE
 #define	__PRINT_IN_GAME_TIME(x, y)	Printing_int(Printing_getInstance(), Clock_getTime(Game_getInGameClock(Game_getInstance())), x, y, NULL);
 
-/*
-#undef __TIMER_RESOLUTION						
-#define __TIMER_RESOLUTION						10
-
-//#define __DEBUG_NO_FADE
-#define __PRINT_FRAMERATE
-#undef	__FRAME_CYCLE
-#define	__FRAME_CYCLE							1
-*/
-/*
-#undef __MEMORY_POOL_ARRAYS																			
-#define __MEMORY_POOL_ARRAYS																			\
-	__BLOCK_DEFINITION(188, 1)																			\
-	__BLOCK_DEFINITION(164, 4)																			\
-	__BLOCK_DEFINITION(132, 30)																			\
-	__BLOCK_DEFINITION(112, 52)																			\
-	__BLOCK_DEFINITION(96, 58)																			\
-	__BLOCK_DEFINITION(76, 32)																			\
-	__BLOCK_DEFINITION(68, 75)																			\
-	__BLOCK_DEFINITION(28, 254)																			\
-	__BLOCK_DEFINITION(20, 632)																			\
-	__BLOCK_DEFINITION(16, 290)	
-*/
-
-/*
-// these improve performance in the real machine
-#undef __OPTICS_NORMALIZE
-#define __OPTICS_NORMALIZE(Vector)													\
-	Vector.x -= (_screenPosition->x + 0x00001000)& 0xFFFFE000;						\
-	Vector.y -= (_screenPosition->y + 0x00001000)& 0xFFFFE000;						\
-	Vector.z -= (_screenPosition->z + 0x00001000)& 0xFFFFE000;
-
-#undef __OPTICS_PROJECT_TO_2D
-#define __OPTICS_PROJECT_TO_2D(Vector3D, Vector2D)									\
-		Vector2D.x = Vector3D.x 													\
-			+ (FIX19_13_MULT(_optical->horizontalViewPointCenter - 					\
-					Vector3D.x, Vector3D.z) >> _optical->maximumViewDistancePower);	\
-		Vector2D.y = Vector3D.y 													\
-			- (FIX19_13_MULT(Vector3D.y - _optical->verticalViewPointCenter,		\
-				Vector3D.z) >> _optical->maximumViewDistancePower);					\
-		Vector2D.z = Vector3D.z;													\
-		Vector2D.x += 0x00001000;													\
-		Vector2D.y += 0x00001000;													\
-		Vector2D.x &= 0xFFFFE000;													\
-		Vector2D.y &= 0xFFFFE000;					
-*/
+#endif
