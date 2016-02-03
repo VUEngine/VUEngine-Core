2388d946e42001b5acd97508203aac2bf9aa301e
Christian Radke <chris@vr32.de>
2016-02-03 07:16:50 -0800

    Minor spelling fix

--- source/base/libgccvb/Mem.c
+++ source/base/libgccvb/Mem.c
@@ -84,7 +84,7 @@ void Mem_clear (u16* dest,  u16 num )
 	for(i = 0; i < num; i++) *dest++ = 0x0000;
 }
 
-void Mem_substract (u8* dest, const u8* src, u16 num, u16 offset,u8 modifier)
+void Mem_subtract (u8* dest, const u8* src, u16 num, u16 offset,u8 modifier)
 {
 }
 
