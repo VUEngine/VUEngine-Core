ffca0f4f5c1f8c84d432847da4553998a2851e28
Jorge Eremiev <jorgech3@gmail.com>
2016-07-12 14:20:24 -0500

    Added sample debug config options.

--- debugConfig.h
+++ debugConfig.h
@@ -1,5 +1,49 @@
 #ifndef	DEBUG_CONFIG_H_
 #define	DEBUG_CONFIG_H_
 
+/*
+// print frame rate
+#define __PRINT_FRAMERATE
+
+// define the timer's resolution used by the physics
+#undef __TIMER_RESOLUTION
+#define __TIMER_RESOLUTION      10
+
+// set  frame rate (0: 50, 1: 25)
+#undef	__FRAME_CYCLE
+#define	__FRAME_CYCLE		    0
+
+// print memory pool's status
+#define __PRINT_MEMORY_POOL_STATUS
+#undef __PRINT_DETAILED_MEMORY_POOL_STATUS
+
+// show game's process profiling
+#define __PROFILING
+
+// alert stack overflows
+#define __ALERT_STACK_OVERFLOW
+
+// define to place the whole memory pool in SRAM
+#define __PUT_MEMORY_POOL_IN_SRAM
+
+// test memory pool's config
+#undef __MEMORY_POOL_ARRAYS
+#define __MEMORY_POOL_ARRAYS																	        \
+	__BLOCK_DEFINITION(192, 1)																			\
+	__BLOCK_DEFINITION(160, 10)																			\
+	__BLOCK_DEFINITION(144, 15)																			\
+	__BLOCK_DEFINITION(136, 35)																			\
+	__BLOCK_DEFINITION(120, 40)																			\
+	__BLOCK_DEFINITION(112, 15)																			\
+	__BLOCK_DEFINITION(104, 10)																			\
+	__BLOCK_DEFINITION(100, 25)																			\
+	__BLOCK_DEFINITION(92, 30)																			\
+	__BLOCK_DEFINITION(84, 30)																			\
+	__BLOCK_DEFINITION(76, 10)																			\
+	__BLOCK_DEFINITION(68, 60)																			\
+	__BLOCK_DEFINITION(28, 300)																			\
+	__BLOCK_DEFINITION(20, 600)																			\
+	__BLOCK_DEFINITION(16, 350)
+*/
 
 #endif
