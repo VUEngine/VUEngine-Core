9c1099ee55a23d3ac7e9419b7466519607666900
Jorge Eremiev <jorgech3@gmail.com>
2016-10-03 15:35:48 -0500

    Updated debug config file.

--- debugConfig.h
+++ debugConfig.h
@@ -1,60 +1,26 @@
 #ifndef	DEBUG_CONFIG_H_
-#define	DEBUG_CONFIG_H_
+#undef	DEBUG_CONFIG_H_
 
 #include <debugUtilities.h>
-
-/*
-// print frame rate
-#define __PRINT_FRAMERATE
-
-// define the timer's resolution used by the physics
-#undef __TIMER_RESOLUTION
-#define __TIMER_RESOLUTION      10
-
-// set  frame rate (0: 50, 1: 25)
-#undef	__FRAME_CYCLE
-#define	__FRAME_CYCLE		    0
-
-// print memory pool's status
-#undef __PRINT_MEMORY_POOL_STATUS
-#undef __PRINT_DETAILED_MEMORY_POOL_STATUS
-
-// show game's process profiling
-#undef __PROFILING
-
-// alert stack overflows
-#undef __ALERT_STACK_OVERFLOW
-
-// alert VPU's overtime
-#define __ALERT_VPU_OVERTIME
-
-// alert transformation - VPU unsync warning
-#undef __ALERT_TRANSFORMATIONS_NOT_IN_SYNC_WITH_VPU
 
 // avoid declaration warnings
 #include <HardwareManager.h>
 void HardwareManager_checkStackStatus(HardwareManager this);
 void HardwareManager_printStackStatus(HardwareManager this, int x, int y, bool resumed);
 
-// test memory pool's config
-#undef __MEMORY_POOL_ARRAYS1
-#define __MEMORY_POOL_ARRAYS1																	        \
-	__BLOCK_DEFINITION(192, 1)																			\
-	__BLOCK_DEFINITION(160, 5)																			\
-	__BLOCK_DEFINITION(144, 6)																			\
-	__BLOCK_DEFINITION(136, 26)																			\
-	__BLOCK_DEFINITION(120, 35)																			\
-	__BLOCK_DEFINITION(112, 12)																			\
-	__BLOCK_DEFINITION(104, 10)																			\
-	__BLOCK_DEFINITION(100, 22)																			\
-	__BLOCK_DEFINITION(92, 24)																			\
-	__BLOCK_DEFINITION(84, 30)																			\
-	__BLOCK_DEFINITION(76, 8)																			\
-	__BLOCK_DEFINITION(68, 60)																			\
-	__BLOCK_DEFINITION(28, 276)																			\
-	__BLOCK_DEFINITION(20, 588)																			\
-	__BLOCK_DEFINITION(16, 276)					    													\
+#undef __PRINT_FRAMERATE
+#undef __PROFILE_GAME
+#undef __PROFILE_GAME_DETAILED
+#undef __PROFILE_STREAMING
+#undef __DIMM_FOR_PROFILING
+#undef __PROFILE_GAME_STATE_DURING_VIP_INTERRUPT
+#undef __ALERT_VIP_OVERTIME
+#undef __RUN_DELAYED_MESSAGES_DISPATCHING_AT_HALF_FRAME_RATE
 
-*/
+#undef __PRINT_MEMORY_POOL_STATUS
+#undef __PRINT_DETAILED_MEMORY_POOL_STATUS
+
+#undef __ALERT_TRANSFORMATIONS_NOT_IN_SYNC_WITH_VIP
+#undef __ALERT_STACK_OVERFLOW
 
 #endif
