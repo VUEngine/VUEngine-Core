7f85f8a807cbfc4a33405b1bcbf12fe9b70fb3e0
Jorge Eremiev <jorgech3@gmail.com>
2017-03-12 13:32:18 -0500

    Commented out non-fundamental debug assert.

--- source/physics/PhysicalWorld.c
+++ source/physics/PhysicalWorld.c
@@ -223,15 +223,17 @@ void PhysicalWorld_unregisterBody(PhysicalWorld this, SpatialObject owner)
 
 	// if the entity is already registered
 	Body body = PhysicalWorld_getBody(this, owner);
+/*
 #ifdef __DEBUG
 	if(!body)
 	{
+		Printing_setDebugMode(Printing_getInstance());
 		Printing_text(Printing_getInstance(), __GET_CLASS_NAME(owner), 1, 15, NULL);
 	}
 
 	ASSERT(body, "PhysicalWorld::unregisterBody: body not found");
 #endif
-
+*/
 	if(body)
 	{
 		// deactivate the shape, will be removed in the next update
