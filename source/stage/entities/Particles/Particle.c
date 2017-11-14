51d07410b46977c601d769d87f26714c42cd1fac
Jorge Eremiev <jorgech3@gmail.com>
2017-11-14 11:02:52 -0500

    Put back call to Sprite's update animation.

--- source/stage/entities/Particles/Particle.c
+++ source/stage/entities/Particles/Particle.c
@@ -176,6 +176,7 @@ u32 Particle_update(Particle this, u32 elapsedTime, void (* behavior)(Particle p
 			return true;
 		}
 
+		Sprite_updateAnimation(__SAFE_CAST(Sprite, this->objectSprite));
 	}
 
 	return false;
