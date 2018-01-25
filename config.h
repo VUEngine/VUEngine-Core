59a9a86de0c99d28a36621bc8e914801d266a24c
Jorge Eremiev <jorgech3@gmail.com>
2018-01-25 17:10:08 -0500

    Increased memory pools to prevent depletion and speed up allocations.

--- config.h
+++ config.h
@@ -190,9 +190,9 @@
 	__BLOCK_DEFINITION(80, 50)																			\
 	__BLOCK_DEFINITION(68, 60)																			\
 	__BLOCK_DEFINITION(40, 30)																			\
-	__BLOCK_DEFINITION(28, 300)																			\
-	__BLOCK_DEFINITION(20, 650)																			\
-	__BLOCK_DEFINITION(16, 350)																			\
+	__BLOCK_DEFINITION(28, 350)																			\
+	__BLOCK_DEFINITION(20, 700)																			\
+	__BLOCK_DEFINITION(16, 450)																			\
 
 #undef __SET_MEMORY_POOL_ARRAYS
 #define __SET_MEMORY_POOL_ARRAYS																		\
