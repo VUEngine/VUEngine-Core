10adecdcd538eff8373cfbb314e97a3341ed3c6a
Jorge Eremiev <jorgech3@gmail.com>
2018-05-08 19:36:58 -0500

    More merges.

--- source/graphics/3d/DirectDraw.c
+++ source/graphics/3d/DirectDraw.c
@@ -176,7 +176,7 @@ void DirectDraw_drawBlackPixel(DirectDraw this __attribute__ ((unused)), u32 buf
  * @param x			Camera x coordinate
  * @param y			Camera y coordinate
  */
-static void DirectDraw_drawBlackPixelWrapper(DirectDraw this __attribute__ ((unused)), u32 buffer, u16 x, u16 y, int color)
+static void DirectDraw_drawBlackPixelWrapper(DirectDraw this __attribute__ ((unused)), u32 buffer, u16 x, u16 y, int color, __attribute__ ((unused)))
 {
 	ASSERT(this, "DirectDraw::drawBlackPixel: null this");
 
