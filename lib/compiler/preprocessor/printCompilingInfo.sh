a21c4b49bab2df762ad3b37258a4061fe198c9fd
Jorge Eremiev <jorgeche@Jorges-MacBook-Pro.local>
2019-02-14 09:49:49 -0500

    Tweaked output.

--- lib/compiler/preprocessor/printCompilingInfo.sh
+++ lib/compiler/preprocessor/printCompilingInfo.sh
@@ -2,20 +2,20 @@
 #
 INPUT_FILE=$1
 
-message="Compiling file:  $INPUT_FILE..."
+message="Compiling file:  $INPUT_FILE"
 
 if [ -z "${INPUT_FILE##*assets/*}" ];
 then
-	message=`sed -e 's#^.*assets/\(.*$\)#Compiling asset: \1...#g' <<< $INPUT_FILE`
+	message=`sed -e 's#^.*assets/\(.*$\)#Compiling asset: \1#g' <<< $INPUT_FILE`
 fi
 
 if [ -z "${INPUT_FILE##*source*}" ];
 then
-	message=`sed -e 's#^.*source[s]*/\(.*$\)#Compiling file:  \1...#g' <<< $INPUT_FILE`
+	message=`sed -e 's#^.*source[s]*/\(.*$\)#Compiling file:  \1#g' <<< $INPUT_FILE`
 else
 	if [ -z "${INPUT_FILE##*object*}" ];
 	then
-		message=`sed -e 's#^.*object[s]*/\(.*$\)#Compiling file:  \1...#g' <<< $INPUT_FILE`
+		message=`sed -e 's#^.*object[s]*/\(.*$\)#Compiling file:  \1#g' <<< $INPUT_FILE`
 	fi
 fi
 
