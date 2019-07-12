aa6e860d3f00dea61cbf85f2508f19b09cffa9ba
Jorge Eremiev <jorgech3@gmail.com>
2019-07-12 11:35:59 -0500

    Added cache management macros just in case a developer may want to use them.

--- source/hardware/HardwareManager.h
+++ source/hardware/HardwareManager.h
@@ -50,6 +50,11 @@ static u8* const _hardwareRegisters =			(u8*)0x02000000;
 #define	__WCR		0x24	// Wait-state Control Register		(0x0200 0024)
 #define	__SCR		0x28	// Serial Control Register			(0x0200 0028)
 
+// cache management
+#define CACHE_ENABLE	asm("mov 2,r1 \n  ldsr r1,sr24": /* No Output */: /* No Input */: "r1" /* Reg r1 Used */)
+#define CACHE_DISABLE	asm("ldsr r0,sr24")
+#define CACHE_CLEAR		asm("mov 1,r1 \n  ldsr r1,sr24": /* No Output */: /* No Input */: "r1" /* Reg r1 Used */)
+
 
 //---------------------------------------------------------------------------------------------------------
 //											CLASS'S DECLARATION
