e7d1a2c9a1abcaab20c64f3e6b1a61ef57d0d4ff
Jorge Eremiev <jorgech3@gmail.com>
2019-07-16 10:06:22 -0500

    Sync particle's graphics before showing them up.

--- source/stage/entities/Particles/Particle.c
+++ source/stage/entities/Particles/Particle.c
@@ -208,6 +208,8 @@ void Particle::show()
 {
 	ASSERT(this->sprite, "Particle::show: null sprite");
 
+	Particle::synchronizeGraphics(this, __INVALIDATE_TRANSFORMATION);
+
 	Sprite::show(this->sprite);
 
 	if(this->particleSpec->initialAnimation && this->particleSpec->animationDescription)
