40e42f362c9f7cf47119dfe3acd359f0554a769f
jorgeche <jorgech3@gmail.com>
2022-05-04 20:39:15 -0500

    Tied game cycle to GAMESTART instead of FRAMESTART.

--- source/hardware/VIPManager.c
+++ source/hardware/VIPManager.c
@@ -262,7 +262,7 @@ static void VIPManager::interruptHandler()
 	HardwareManager::disableMultiplexedInterrupts();
 
 	// enable interrupts
-	VIPManager::enableInterrupts(_vipManager, __FRAMESTART | __XPEND);
+	VIPManager::enableInterrupts(_vipManager, __GAMESTART | __XPEND);
 }
 
 /**
@@ -275,6 +275,7 @@ void VIPManager::processInterrupt(uint16 interrupt)
 	static uint16 interruptTable[] =
 	{
 		__FRAMESTART,
+		__GAMESTART,
 		__XPEND,
 		__TIMEERR,
 		__SCANERR
@@ -287,6 +288,10 @@ void VIPManager::processInterrupt(uint16 interrupt)
 		switch(interrupt & interruptTable[i])
 		{
 			case __FRAMESTART:
+
+				break;
+
+			case __GAMESTART:
 
 				if(_vipManager->processingFRAMESTART)
 				{
@@ -363,7 +368,7 @@ void VIPManager::processInterrupt(uint16 interrupt)
 				}
 
 				// Allow frame start interrupt
-				VIPManager::enableInterrupts(this, __FRAMESTART);
+				VIPManager::enableInterrupts(this, __GAMESTART);
 
 				// Do not remove this, it prevents
 				// graphical glitches when the VIP
