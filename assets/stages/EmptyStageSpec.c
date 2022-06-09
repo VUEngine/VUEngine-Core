3808ec376a6b0d9f0ba9fa8a46c9afd26554ee6d
jorgeche <jorgech3@gmail.com>
2022-06-09 02:22:54 -0500

    Fixed warning.

--- assets/stages/EmptyStageSpec.c
+++ assets/stages/EmptyStageSpec.c
@@ -180,7 +180,7 @@ StageROMSpec EmptyStageSpec =
 		{
 			// maximum view distance's power into the horizon
 			__MAXIMUM_X_VIEW_DISTANCE, __MAXIMUM_Y_VIEW_DISTANCE,
-			// distance from left to right eye (depth sensation)
+			// distance of the eyes to the screen
 			__DISTANCE_EYE_SCREEN,
 			// distance from left to right eye (depth sensation)
 			__BASE_FACTOR,
