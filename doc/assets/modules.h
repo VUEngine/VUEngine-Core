3a9b00df09735ec1a6145d369d50171d86166a10
Jorge Eremiev <jorgech3@gmail.com>
2024-11-24 23:41:21 -0500

    Documentation.

--- doc/assets/modules.h
+++ doc/assets/modules.h
@@ -1,104 +1,104 @@
-/** @defgroup base Base
+/* @defgroup base Base
  *  @brief Base classes
  */
 
- /** @defgroup base-libgccvb libgccvb
+ /* @defgroup base-libgccvb libgccvb
  *  @ingroup base
  *  @brief libgccvb classes
  */
 
 
-/** @defgroup graphics Graphics
+/* @defgroup graphics Graphics
  *  @brief Graphics classes
  */
 
-/** @defgroup graphics-2d 2D Graphics
+/* @defgroup graphics-2d 2D Graphics
  *  @ingroup graphics
  *  @brief 2D Graphics classes
  */
 
-/** @defgroup graphics-2d-sprites Sprites
+/* @defgroup graphics-2d-sprites Sprites
  *  @ingroup graphics-2d
  *  @brief Sprites classes
  */
 
-/** @defgroup graphics-2d-sprites-animation Sprite Animation
+/* @defgroup graphics-2d-sprites-animation Sprite Animation
  *  @ingroup graphics-2d-sprites
  *  @brief Sprite Animation classes
  */
 
-/** @defgroup graphics-2d-sprites-bgmap BGMap Sprites
+/* @defgroup graphics-2d-sprites-bgmap BGMap Sprites
  *  @ingroup graphics-2d-sprites
  *  @brief BGMap Sprite classes
  */
 
-/** @defgroup graphics-2d-sprites-object Object Sprites
+/* @defgroup graphics-2d-sprites-object Object Sprites
  *  @ingroup graphics-2d-sprites
  *  @brief Object Sprite classes
  */
 
-/** @defgroup graphics-2d-textures Textures
+/* @defgroup graphics-2d-textures Textures
  *  @ingroup graphics-2d
  *  @brief Textures classes
  */
 
-/** @defgroup graphics-2d-textures-bgmap BGMap Textures
+/* @defgroup graphics-2d-textures-bgmap BGMap Textures
  *  @ingroup graphics-2d-textures
  *  @brief BGMap Texture classes
  */
 
-/** @defgroup graphics-2d-textures-char Texture Chars
+/* @defgroup graphics-2d-textures-char Texture Chars
  *  @ingroup graphics-2d-textures
  *  @brief Texture Chars classes
  */
 
-/** @defgroup graphics-2d-textures-object Object Textures
+/* @defgroup graphics-2d-textures-object Object Textures
  *  @ingroup graphics-2d-textures
  *  @brief Object Texture classes
  */
 
-/** @defgroup graphics-3d 3D Graphics
+/* @defgroup graphics-3d 3D Graphics
  *  @ingroup graphics
  *  @brief 3D Graphics classes
  */
 
 
-/** @defgroup hardware Hardware
+/* @defgroup hardware Hardware
  *  @brief Hardware classes
  */
 
 
-/** @defgroup physics Physics
+/* @defgroup physics Physics
  *  @brief Physics classes
  */
 
 
-/** @defgroup camera Camera
+/* @defgroup camera Camera
  *  @brief Camera classes
  */
 
 
-/** @defgroup stage Stage
+/* @defgroup stage Stage
  *  @brief Stage classes
  */
 
-/** @defgroup stage-entities Entities
+/* @defgroup stage-entities Entities
  *  @ingroup stage
  *  @brief Entity classes
  */
 
-/** @defgroup stage-entities-particles Particles
+/* @defgroup stage-entities-particles Particles
  *  @ingroup stage
  *  @brief Particle classes
  */
 
 
-/** @defgroup states States
+/* @defgroup states States
  *  @brief States classes
  */
 
 
-/** @defgroup tools Tools
+/* @defgroup tools Tools
  *  @brief Tools classes
  */
 
