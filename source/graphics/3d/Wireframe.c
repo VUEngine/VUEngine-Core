6213da4da5b1b7ffd15fcc940d038c892c17bd6f
Jorge Eremiev <jorgech3@gmail.com>
2024-12-11 23:42:08 -0500

    Removed some culling checks from Wireframe::prepareForRender that are too aggresive.

--- source/graphics/3d/Wireframe.c
+++ source/graphics/3d/Wireframe.c
@@ -104,16 +104,6 @@ bool Wireframe::prepareForRender(Vector3D* relativePosition)
 
 	NM_ASSERT(NULL != ((WireframeSpec*)this->componentSpec), "Wireframe::setupRenderingMode: NULL wireframeSpec");
 
-	if(0 > Vector3D::dotProduct(*relativePosition, _cameraDirection))
-	{
-		this->interlaced = ((WireframeSpec*)this->componentSpec)->interlaced;
-		this->color = __COLOR_BLACK;
-#ifdef __WIREFRAME_MANAGER_SORT_FOR_DRAWING
-		this->squaredDistanceToCamera = __WIREFRAME_MAXIMUM_SQUARE_DISTANCE_TO_CAMERA;
-#endif
-		return __COLOR_BLACK != this->color;
-	}
-
 	this->color = ((WireframeSpec*)this->componentSpec)->color;
 
 	fixed_ext_t distanceToCamera = Vector3D::squareLength(*relativePosition);
@@ -129,32 +119,6 @@ bool Wireframe::prepareForRender(Vector3D* relativePosition)
 	}
 	else
 	{
-		int16 cameraViewingAngle = __CAMERA_VIEWING_ANGLE;
-
-		if(0 == distanceToCamera)
-		{
-			this->color = __COLOR_BLACK;
-#ifdef __WIREFRAME_MANAGER_SORT_FOR_DRAWING
-			this->squaredDistanceToCamera = __WIREFRAME_MAXIMUM_SQUARE_DISTANCE_TO_CAMERA;
-#endif
-			return __COLOR_BLACK != this->color;
-		}
-
-		if(__FIXED_SQUARE(__PIXELS_TO_METERS(__SCREEN_WIDTH << 1)) > distanceToCamera)
-		{
-			// This is a hack. The correct operation should use a fixed div and a conversion to int
-			// but this is way faster and works accurately enough
-			cameraViewingAngle += (__FIXED_SQUARE(__PIXELS_TO_METERS(__SCREEN_WIDTH << 2)) / distanceToCamera);
-		}
-		
-		if(__COS(cameraViewingAngle) > __FIXED_EXT_TO_FIX7_9(Vector3D::dotProduct(Vector3D::normalize(*relativePosition), _cameraDirection)))
-		{
-#ifdef __WIREFRAME_MANAGER_SORT_FOR_DRAWING
-			this->squaredDistanceToCamera = __WIREFRAME_MAXIMUM_SQUARE_DISTANCE_TO_CAMERA;
-#endif
-			this->color = __COLOR_BLACK;
-			return __COLOR_BLACK != this->color;
-		}
 
 		if(__FIXED_SQUARE((__DIRECT_DRAW_INTERLACED_THRESHOLD << 1) < distanceToCamera))
 		{
