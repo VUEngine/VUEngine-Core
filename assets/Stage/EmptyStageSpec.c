5194914b7964d35e8adede1a951c4f1444085ba1
Jorge Eremiev <jorgech3@gmail.com>
2024-12-25 18:54:04 -0500

    Standarized stage specs comments.

--- assets/Stage/EmptyStageSpec.c
+++ assets/Stage/EmptyStageSpec.c
@@ -21,7 +21,7 @@
 
 StageROMSpec EmptyStageSpec =
 {
-	// allocator
+	// Class allocator
 	__TYPE(Stage),
 
 	// Timer config
@@ -37,9 +37,9 @@ StageROMSpec EmptyStageSpec =
 		false
 	},
 
-	// level
+	// General stage's attributes
 	{
-		// Size
+		// Stage's size in pixels
 		{
 			// x
 			__SCREEN_WIDTH,
@@ -49,7 +49,7 @@ StageROMSpec EmptyStageSpec =
 			__SCREEN_DEPTH,
 		},
 
-		// camera's initial position inside the game world
+		// Camera's initial position inside the stage
 		{
 			// x
 			0,
@@ -61,7 +61,7 @@ StageROMSpec EmptyStageSpec =
 			0,
 		},
 
-		// camera's frustum
+		// Camera's frustum
         {
         	// x0
         	0,
@@ -78,79 +78,79 @@ StageROMSpec EmptyStageSpec =
         }
 	},
 
-	// streaming
+	// Streaming
 	{
-		// load padding
+		// Padding to be added to camera's frustum when checking if a entity spec
+		// describes an entity that is within the camera's range
 		40,
-		// unload padding
+		// Padding to be added to camera's frustum when checking if a entity is
+		// out of the camera's range
 		16,
-		// streaming amplitude
+		// Amount of entity descriptions to check for streaming in entities
 		24,
-		// particle removal delay cycles
-		0,
-		// deferred
+		// If true, entity instantiation is done over time
 		false,
 	},
 
-	// rendering
+	// Rendering
 	{
-		// maximum number of texture's rows to write each time the texture writing is active
+		// Maximum number of texture's rows to write each time the texture writing is active
 		12,
 
-		// maximum number of rows to compute on each call to the affine functions
+		// Maximum number of rows to compute on each call to the affine functions
 		16,
 
-		// colors config
+		// Color configuration
 		{
-			// background color
+			// Background color
 			__COLOR_BLACK,
 
-			// brightness
-			// these values times the repeat values specified in the column table (max. 16) make the final
+			// Brightness
+			// These values times the repeat values specified in the column table (max. 16) make the final
 			// brightness values on the respective regions of the screen. maximum brightness is 128.
 			{
-				// dark red
+				// Dark red
 				16,
-				// medium red
+				// Medium red
 				__BRIGHTNESS_MEDIUM_RED,
-				// bright red
+				// Bright red
 				__BRIGHTNESS_BRIGHT_RED,
 			},
 
-			// brightness repeat
+			// Brightness repeat
 			(BrightnessRepeatSpec*)NULL,
 		},
 
-		// palettes' config
+		// Palettes' configuration
 		{
 			{
-				// bgmap palette 0
+				// Bgmap palette 0
 				__BGMAP_PALETTE_0,
-				// bgmap palette 1
+				// Bgmap palette 1
 				__BGMAP_PALETTE_1,
-				// bgmap palette 2
+				// Bgmap palette 2
 				__BGMAP_PALETTE_2,
-				// bgmap palette 3
+				// Bgmap palette 3
 				__BGMAP_PALETTE_3,
 			},
 			{
-				// object palette 0
+				// Object palette 0
 				__OBJECT_PALETTE_0,
-				// object palette 1
+				// Object palette 1
 				__OBJECT_PALETTE_1,
-				// object palette 2
+				// Object palette 2
 				__OBJECT_PALETTE_2,
-				// object palette 3
+				// Object palette 3
 				__OBJECT_PALETTE_3,
 			},
 		},
 
-		// bgmap segments configuration
-		// number of segments reserved for the param table
+		// Bgmap segments configuration
+		// Number of BGMAP segments reserved for the param
 		1,
 
-		// object segments sizes (up to 1024 in total)
-		// can impact performance, make sure to configure only as large as maximally needed
+		// Object segments' sizes (__spt0 to __spt3, up to 1024 in total)
+		// Can impact performance, make sure to configure only as large as maximally needed
 		{
 			// __spt0
 			0,
@@ -162,8 +162,8 @@ StageROMSpec EmptyStageSpec =
 			0,
 		},
 
-		// object segments z coordinates
-		// note that each spt's z coordinate much be larger than or equal to the previous one's,
+		// Object segments' z coordinates (__spt0 to __spt3)
+		// Note that each spt's z coordinate much be larger than or equal to the previous one's,
 		// since the vip renders obj worlds in reverse order (__spt3 to __spt0)
 		{
 			// __spt0
@@ -176,63 +176,63 @@ StageROMSpec EmptyStageSpec =
 			0,
 		},
 
-		// optical configuration values
+		// Struct defining the optical settings for the stage
 		{
-			// maximum view distance's power into the horizon
+			// Maximum view distance's power into the horizon
 			__MAXIMUM_X_VIEW_DISTANCE, __MAXIMUM_Y_VIEW_DISTANCE,
-			// distance of the eyes to the screen
+			// Distance of the eyes to the screen
 			__CAMERA_NEAR_PLANE,
-			// distance from left to right eye (depth sensation)
+			// Distance from left to right eye (depth sensation)
 			__BASE_FACTOR,
-			// horizontal view point center
+			// Horizontal view point center
 			__HORIZONTAL_VIEW_POINT_CENTER,
-			// vertical view point center
+			// Vertical view point center
 			__VERTICAL_VIEW_POINT_CENTER,
-			// scaling factor
+			// Scaling factor
 			__SCALING_MODIFIER_FACTOR,
 		},
 	},
 
-	// physics
+	// Physical world's properties
 	{
-		// gravity
+		// Gravity
 		{
 			__I_TO_FIXED(0),
 			__F_TO_FIXED(0),
 			__I_TO_FIXED(0),
 		},
 
-		// Friction
+		// Friction coefficient
 		__F_TO_FIXED(0),
 	},
 
-	// assets
+	// Assets
 	{
-		// fonts to preload
+		// Fonts to preload
 		(FontSpec**)NULL,
 
-		// char sets to preload
+		// CharSets to preload
 		(CharSetSpec**)NULL,
 
-		// textures to preload
+		// Textures to preload
 		(TextureSpec**)NULL,
 
 		// background sounds
 		(SoundSpec**)NULL,
 	},
 
-	// entities
+	// Entities
 	{
-		// ui
+		// UI configuration
 		{
 			(PositionedEntity*)NULL,
 			__TYPE(UIContainer),
 		},
 
-		// Children specs
+		// Stage's children entities
 		(PositionedEntity*)NULL,
 	},
 
-	// post processing effects
+	// Post processing effects
 	(PostProcessingEffect*)NULL,
 };
