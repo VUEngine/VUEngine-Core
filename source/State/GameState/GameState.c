58c839bad6ddfd7ac2ca1b607488d5f7184a8d84
Jorge Eremiev <jorgech3@gmail.com>
2025-01-18 15:03:16 -0500

    Fixed GameState::createManagers.

--- source/State/GameState/GameState.c
+++ source/State/GameState/GameState.c
@@ -661,14 +661,17 @@ void GameState::createManagers()
 {
 	for(int16 i = 0; i < kComponentTypes; i++)
 	{
-		AllocatorPointer _componentManagerAllocators[kComponentTypes];
-		_componentManagerAllocators[kBehaviorComponent] = __TYPE(BehaviorManager);
-		_componentManagerAllocators[kPhysicsComponent] = __TYPE(BodyManager);
-		_componentManagerAllocators[kColliderComponent] = __TYPE(ColliderManager);
-		_componentManagerAllocators[kSpriteComponent] = __TYPE(SpriteManager);
-		_componentManagerAllocators[kWireframeComponent] = __TYPE(WireframeManager);
+		if(NULL == this->componentManagers[i])
+		{
+			AllocatorPointer _componentManagerAllocators[kComponentTypes];
+			_componentManagerAllocators[kBehaviorComponent] = __TYPE(BehaviorManager);
+			_componentManagerAllocators[kPhysicsComponent] = __TYPE(BodyManager);
+			_componentManagerAllocators[kColliderComponent] = __TYPE(ColliderManager);
+			_componentManagerAllocators[kSpriteComponent] = __TYPE(SpriteManager);
+			_componentManagerAllocators[kWireframeComponent] = __TYPE(WireframeManager);
 
-		this->componentManagers[i] = ComponentManager::safeCast(_componentManagerAllocators[i]());
+			this->componentManagers[i] = ComponentManager::safeCast(_componentManagerAllocators[i]());
+		}
 	}
 }
 
