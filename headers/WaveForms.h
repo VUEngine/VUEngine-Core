399403d58a79ff3331d1810a581200c9c9e0e249
Christian Radke <c.radke@posteo.de>
2025-02-07 15:54:35 +0100

    Update item files

--- headers/WaveForms.h
+++ headers/WaveForms.h
@@ -1,17 +1,18 @@
-///////////////////////////////////////////////////////////////////////////////////////////////////////////
-//										THIS FILE WAS AUTO-GENERATED - DO NOT EDIT										 //
-///////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////  THIS FILE WAS AUTO-GENERATED - DO NOT EDIT  ///////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 #ifndef WAVEFORMS_H_
 #define WAVEFORMS_H_
 
 //——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
-// INCLUDES
+// DECLARATIONS
 //——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
 
 extern const int8 GlockenWaveForm[];
 extern const int8 LinearWaveForm[];
 extern const int8 OrganWaveForm[];
+extern const int8 PCMWaveForm[];
 extern const int8 PianoWaveForm[];
 extern const int8 SawSquareWaveForm[];
 extern const int8 SawtoothWaveForm[];
@@ -22,6 +23,5 @@ extern const int8 SquareDuty75WaveForm[];
 extern const int8 TriangleWaveForm[];
 extern const int8 TrumpetWaveForm[];
 extern const int8 ViolinWaveForm[];
-extern const int8 PCMWaveForm[];
 
 #endif
