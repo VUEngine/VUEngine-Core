f7f679fae87565ccc3442963e038823ad58e6382
Christian Radke <c.radke@posteo.de>
2025-02-07 15:51:31 +0100

    Update EngineConfig type

--- headers/Config.h
+++ headers/Config.h
@@ -13,10 +13,6 @@
 #include "RomInfo.h"
 
 //——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
-// MACROS
-//——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
-
-//——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
 // GAME ENTRY POINT
 //——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
 
@@ -29,18 +25,6 @@
 //——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
 
 #define __FIXED_POINT_TYPE 6
-
-//——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
-// COMMUNICATIONS
-//——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
-
-#define __ENABLE_COMMUNICATIONS
-
-//——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
-// DEBUGGING / PROFILING
-//——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
-
-#define __STACK_HEADROOM 500
 
 //——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
 // DEBUGGING TOOLS
@@ -84,18 +68,18 @@
 // OPTICS / PROJECTION
 //——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
 
-#define __LEGACY_COORDINATE_PROJECTION
+#undef __LEGACY_COORDINATE_PROJECTION
 
 #define __SCREEN_WIDTH				   384
 #define __SCREEN_HEIGHT				   224
 #define __SCREEN_DEPTH				   2048
 
-#define __MAXIMUM_X_VIEW_DISTANCE	   1024
-#define __MAXIMUM_Y_VIEW_DISTANCE	   1024
+#define __MAXIMUM_X_VIEW_DISTANCE	   2048
+#define __MAXIMUM_Y_VIEW_DISTANCE	   4096
 
 #define __CAMERA_NEAR_PLANE			   0
 
-#define __BASE_FACTOR				   21
+#define __BASE_FACTOR				   32
 
 #define __HORIZONTAL_VIEW_POINT_CENTER 192
 #define __VERTICAL_VIEW_POINT_CENTER   112
@@ -114,14 +98,12 @@
 #define __GAME_FRAME_DURATION		   (__MILLISECONDS_PER_SECOND / __TARGET_FPS)
 #define __OPTIMUM_FPS				   (__TARGET_FPS >> __FRAME_CYCLE)
 
-#define __RUN_DELAYED_MESSAGES_DISPATCHING_AT_HALF_FRAME_RATE
-
 //——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
 // ANIMATION
 //——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
 
-#define __MAX_ANIMATION_FUNCTION_NAME_LENGTH 10
-#define __MAX_FRAMES_PER_ANIMATION_FUNCTION	 241
+#define __MAX_ANIMATION_FUNCTION_NAME_LENGTH 16
+#define __MAX_FRAMES_PER_ANIMATION_FUNCTION	 16
 
 //——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
 // MEMORY POOL
@@ -130,39 +112,35 @@
 #undef __MEMORY_POOL_CLEAN_UP
 
 #undef __MEMORY_POOLS
-#define __MEMORY_POOLS 13
+#define __MEMORY_POOLS 11
 
 #undef __MEMORY_POOL_ARRAYS
 #define __MEMORY_POOL_ARRAYS \
-	__BLOCK_DEFINITION(356, 1) \
-	__BLOCK_DEFINITION(332, 8) \
-	__BLOCK_DEFINITION(188, 1) \
-	__BLOCK_DEFINITION(168, 25) \
-	__BLOCK_DEFINITION(144, 20) \
-	__BLOCK_DEFINITION(112, 25) \
-	__BLOCK_DEFINITION(100, 50) \
-	__BLOCK_DEFINITION(84, 55) \
-	__BLOCK_DEFINITION(48, 90) \
-	__BLOCK_DEFINITION(32, 190) \
-	__BLOCK_DEFINITION(20, 965) \
-	__BLOCK_DEFINITION(16, 265) \
-	__BLOCK_DEFINITION(12, 265) \
+	__BLOCK_DEFINITION(164, 1) \
+	__BLOCK_DEFINITION(152, 10) \
+	__BLOCK_DEFINITION(140, 10) \
+	__BLOCK_DEFINITION(116, 40) \
+	__BLOCK_DEFINITION(108, 40) \
+	__BLOCK_DEFINITION(80, 50) \
+	__BLOCK_DEFINITION(68, 60) \
+	__BLOCK_DEFINITION(40, 30) \
+	__BLOCK_DEFINITION(28, 350) \
+	__BLOCK_DEFINITION(20, 700) \
+	__BLOCK_DEFINITION(16, 450) \
 
 #undef __SET_MEMORY_POOL_ARRAYS
 #define __SET_MEMORY_POOL_ARRAYS \
-	__SET_MEMORY_POOL_ARRAY(356) \
-	__SET_MEMORY_POOL_ARRAY(332) \
-	__SET_MEMORY_POOL_ARRAY(188) \
-	__SET_MEMORY_POOL_ARRAY(168) \
-	__SET_MEMORY_POOL_ARRAY(144) \
-	__SET_MEMORY_POOL_ARRAY(112) \
-	__SET_MEMORY_POOL_ARRAY(100) \
-	__SET_MEMORY_POOL_ARRAY(84) \
-	__SET_MEMORY_POOL_ARRAY(48) \
-	__SET_MEMORY_POOL_ARRAY(32) \
+	__SET_MEMORY_POOL_ARRAY(164) \
+	__SET_MEMORY_POOL_ARRAY(152) \
+	__SET_MEMORY_POOL_ARRAY(140) \
+	__SET_MEMORY_POOL_ARRAY(116) \
+	__SET_MEMORY_POOL_ARRAY(108) \
+	__SET_MEMORY_POOL_ARRAY(80) \
+	__SET_MEMORY_POOL_ARRAY(68) \
+	__SET_MEMORY_POOL_ARRAY(40) \
+	__SET_MEMORY_POOL_ARRAY(28) \
 	__SET_MEMORY_POOL_ARRAY(20) \
 	__SET_MEMORY_POOL_ARRAY(16) \
-	__SET_MEMORY_POOL_ARRAY(12) \
 
 #define __MEMORY_POOL_WARNING_THRESHOLD 85
 
@@ -183,17 +161,17 @@
 //——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
 
 #define __TOTAL_LAYERS					32
-#define __TOTAL_OBJECTS					256
-#undef __SPRITE_ROTATE_IN_3D
+#define __TOTAL_OBJECTS					1024
+#define __SPRITE_ROTATE_IN_3D
 #define __HACK_BGMAP_SPRITE_HEIGHT
 
 //——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
 // TEXTURE MANAGEMENT
 //——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
 
-#define __TOTAL_NUMBER_OF_BGMAPS_SEGMENTS 14
-#define __PARAM_TABLE_SEGMENTS			  0
-#define __NUM_BGMAPS_PER_SEGMENT		  10
+#define __TOTAL_NUMBER_OF_BGMAPS_SEGMENTS 10
+#define __PARAM_TABLE_SEGMENTS			  1
+#define __NUM_BGMAPS_PER_SEGMENT		  14
 #define __MAX_NUMBER_OF_BGMAPS_SEGMENTS	  (__TOTAL_NUMBER_OF_BGMAPS_SEGMENTS - __PARAM_TABLE_SEGMENTS)
 
 #define __PRINTING_BGMAP_X_OFFSET		  0
@@ -213,8 +191,8 @@
 //——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
 
 #define __GRAVITY						  10.0f
-#define __MAXIMUM_FRICTION_COEFFICIENT	  __I_TO_FIXED(10)
-#define __PHYSICS_TIME_ELAPSED_DIVISOR	  1
+#define __MAXIMUM_FRICTION_COEFFICIENT	  __I_TO_FIXED(1)
+#define __PHYSICS_TIME_ELAPSED_DIVISOR	  2
 #undef __PHYSICS_HIGH_PRECISION
 #define __STOP_VELOCITY_THRESHOLD				 __PIXELS_TO_METERS(8)
 #define __STOP_BOUNCING_VELOCITY_THRESHOLD		 __PIXELS_TO_METERS(48)
@@ -227,8 +205,8 @@
 // SOUND
 //——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
 
-#define __EAR_DISPLACEMENT						 192
-#define __SOUND_STEREO_ATTENUATION_DISTANCE		 512
+#define __EAR_DISPLACEMENT						 384
+#define __SOUND_STEREO_ATTENUATION_DISTANCE		 2048
 
 //——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
 // BRIGHTNESS
@@ -238,24 +216,24 @@
 #define __BRIGHTNESS_MEDIUM_RED					 64
 #define __BRIGHTNESS_BRIGHT_RED					 128
 
-#define __FADE_DELAY							 50
-#define __CAMERA_EFFECT_FADE_INCREMENT			 8
+#define __FADE_DELAY							 16
+#define __CAMERA_EFFECT_FADE_INCREMENT			 1
 
 //——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
 // COLOR PALETTES
 //——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
 
-#define __PRINTING_PALETTE						 3
+#define __PRINTING_PALETTE						 0
 
 #define __BGMAP_PALETTE_0						 0xE4  // 11100100
 #define __BGMAP_PALETTE_1						 0xE0  // 11100000
-#define __BGMAP_PALETTE_2						 0xD0  // 11010000
-#define __BGMAP_PALETTE_3						 0x90  // 10010000
+#define __BGMAP_PALETTE_2						 0x90  // 10010000
+#define __BGMAP_PALETTE_3						 0x50  // 01010000
 
 #define __OBJECT_PALETTE_0						 0xE4  // 11100100
 #define __OBJECT_PALETTE_1						 0xE0  // 11100000
-#define __OBJECT_PALETTE_2						 0xD0  // 11010000
-#define __OBJECT_PALETTE_3						 0x90  // 10010000
+#define __OBJECT_PALETTE_2						 0x90  // 10010000
+#define __OBJECT_PALETTE_3						 0x50  // 01010000
 
 //——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
 // RANDOM NUMBER GENERATION
