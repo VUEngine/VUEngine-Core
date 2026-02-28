a022127fd85004ff6b3406e6fcf248620be75b91
Jorge Eremiev <jorgech3@gmail.com>
2026-02-28 10:15:25 -0500

    Implemented support for Lemur's Terminal.

---
+++ source/Debugging/Terminal/Terminal.h
@@ -0,0 +1,37 @@
+/*
+ * VUEngine Core
+ *
+ * © Jorge Eremiev <jorgech3@gmail.com> and Christian Radke <c.radke@posteo.de>
+ *
+ * For the full copyright and license information, please view the LICENSE file
+ * that was distributed with this source code.
+ */
+
+#ifndef TERMINAL_H_
+#define TERMINAL_H_
+
+//——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
+// INCLUDES
+//——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
+
+#include <Object.h>
+
+//——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
+// CLASS' DECLARATION
+//——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
+
+/// Class Terminal
+///
+/// Inherits from Object
+///
+/// Allows printing to supporting emulators.
+static class Terminal : Object
+{
+	/// @publicsection
+
+	/// Prints the provided text to the emulator's Terminal.
+	/// @param text: Pointer to the string to print
+	static void print(char* text);
+}
+
+#endif
